81f3e05cd3163d668336268c75e46ff840678c85
Saurav Patel <patel.saurav.b@gmail.com>
2018-05-27 13:49:08 +0530

    Changes made to book.java
    Removed some code, check .

--- BooksDB/src/book.java
+++ BooksDB/src/book.java
@@ -22,16 +22,6 @@ import java.sql.*;
  * Servlet implementation class book
  */
 @WebServlet("/book")
-public class book extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public book() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
